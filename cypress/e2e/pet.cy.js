4b6a6e6cc003996c62ac79496f9df50f3afe5de7
vaniaportho <vaniaportho@gmail.com>
2024-08-20 22:21:29 -0300

    Lista Pets Data Driven

--- cypress/e2e/pet.cy.js
+++ cypress/e2e/pet.cy.js
@@ -1,5 +1,7 @@
 // Bibliotecas 
 import pet from '../fixtures/pet.json'
+import petAlterado from '../fixtures/petAlterado.json'
+import listaPets from '../fixtures/listaPets.json'
 
 // describe grupo de funções
 // () só será preenchido quando for usar massa
@@ -29,7 +31,7 @@ describe('CRUD da entidade Pet', () => {
   it('GET Pet', () => {
     cy.request({
       method: 'GET',
-      url: `/pet/${pet.id}`,      
+      url: `/pet/${pet.id}`,
     }).then(({ status, body }) => {
       expect(status).to.eq(200)
       expect(body.id).to.eq(30497001)
@@ -39,15 +41,59 @@ describe('CRUD da entidade Pet', () => {
       expect(body.tags[0].id).to.eq(pet.tags[0].id)
       expect(body.tags[0].name).to.eq(pet.tags[0].name)
       expect(body.status).to.eq(pet.status)
-    })     
+    })
 
   }) // termina GET
 
-
   // Put Pet
-
+  it('Put Pet', () => {
+    cy.request({
+      method: 'PUT',
+      url: '/pet',
+      body: petAlterado
+    }).then(({ status, body }) => {
+      expect(status).to.eq(200)
+      expect(body.id).to.eq(30497001)
+      expect(body.name).to.eq(petAlterado.name)
+      expect(body.category.id).to.eq(petAlterado.category.id)
+      expect(body.category.name).to.eq(petAlterado.category.name)
+      expect(body.tags[0].id).to.eq(petAlterado.tags[0].id)
+      expect(body.tags[0].name).to.eq(petAlterado.tags[0].name)
+      expect(body.status).to.eq(petAlterado.status)
+    })
+  }) // termina PUT
 
   // Delete Pet
+  it('DELETE Pet', () => {
+    cy.request({
+      method: 'DELETE',
+      url: `/pet/${pet.id}`
+    }).then(({ status, body }) => {
+      expect(status).to.eq(200) // status code: comunicação com a api
+      expect(body.code).to.eq(200) // process code: processamento na api
+      expect(body.type).to.eq('unknown') // tipo desconhecido padrão
+      expect(body.message).to.eq(`${pet.id}`) // codigo do animal que foi excluído
+    })
+  }) // termina DELETE
 
+  listaPets.forEach((element) => {
+    // Post Pet Data Driven
+    it(`POST Pet Data Driven - ${element.name}`, () => {
+      cy.request({
+        method: 'POST',
+        url: '/pet', // endpoint
+        body: element
+      }).then(({ status, body }) => {
+        expect(status).to.eq(200)
+        expect(body.id).to.eq(element.id)
+        expect(body.name).to.eq(element.name)
+        expect(body.category.id).to.eq(element.category.id)
+        expect(body.category.name).to.eq(element.category.name)
+        expect(body.tags[0].id).to.eq(element.tags[0].id)
+        expect(body.tags[0].name).to.eq(element.tags[0].name)
+        expect(body.status).to.eq(element.status)
+      })
+    }) // termina POST Data Driven
+  })// termina o uso do listaPets
 }) // termina a describe
 
